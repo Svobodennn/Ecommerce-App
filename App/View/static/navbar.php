3a808a1bf7ad19540a15764b0aabeff6b45a4eae
Svobodennn <melih.sarac@hotmail.com>
2023-12-15 00:36:22 +0300

    Sepet görüntüleme eklendi

--- App/View/static/navbar.php
+++ App/View/static/navbar.php
@@ -6,17 +6,22 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
-
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
-
-        <li class="nav-item dropdown">
+        <li  class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge"> ÜRÜN SAYISI </span>
+                <?php
+                $count = 0;
+                    foreach ($cartList as $product){
+                        $count += $product['quantity'];
+                    }
+                ?>
+                <span id="urunSayisi" class="badge badge-danger navbar-badge"> <?= $count ?>  </span>
             </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <div id="cartContainer" class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <?php foreach ($cartList as $product): ?>
                 <a href="#" class="dropdown-item">
                     <!-- Message Start -->
                     <div class="media">
@@ -24,17 +29,17 @@
                         <img src="https://picsum.photos/id/63/100/100" alt="User Avatar" class="img-size-50 mr-3 img-circle">
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
-                                ÜRÜN ADI
+                                <?= $product['title'] ?>
                                 <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                             </h3>
-                            <p class="text-sm">ÜRÜN AÇIKLAMA</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> ÜRÜN ADEDİ</p>
+                            <p class="text-sm"><?= $product['description'] ?></p>
+                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> <?= $product['quantity'] ?></p>
                         </div>
                     </div>
                     <!-- Message End -->
                 </a>
                 <div class="dropdown-divider"></div>
-
+                <?php endforeach; ?>
                 <a href="#" class="dropdown-item dropdown-footer">SİPARİŞİ GÖRÜNTÜLE</a>
             </div>
         </li>
@@ -47,3 +52,6 @@
 </nav>
 <!-- /.navbar -->
 
+<script>
+
+</script>

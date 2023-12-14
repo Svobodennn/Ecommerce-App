11cd1035f2b303451ba6cc3fe5f6383a5d37a6cc
Svobodennn <melih.sarac@hotmail.com>
2023-12-14 22:41:54 +0300

    sepete ekleme routing

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -5,9 +5,12 @@ $cms->router->before('GET|POST', '/', 'Middlewares\MiddlewareAuth@isLogin');
 
 $cms->router->get('/', 'Controllers\Home@Index');
 
+$cms->router->post('/addToCart', 'Controllers\Home@AddToCart');
+
 // Login Page
 $cms->router->get('/login', 'Controllers\Auth@Index');
 // Auth
 $cms->router->post('/login', 'Controllers\Auth@Login');
 $cms->router->get('/logout', 'Controllers\Auth@Logout');
+
 

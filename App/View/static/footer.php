fa52ec7bc1c1bbd9cac8f5e0738983c94a55590e
Svobodennn <melih.sarac@hotmail.com>
2023-12-15 00:32:04 +0300

    moved script files to header

--- App/View/static/footer.php
+++ App/View/static/footer.php
@@ -11,21 +11,3 @@
 
 <!-- REQUIRED SCRIPTS -->
 
-<!-- jQuery -->
-<script src="<?= asset('plugins/jquery/jquery.min.js') ?>"></script>
-<!-- Bootstrap 4 -->
-<script src="<?= asset('plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-<!-- AdminLTE App -->
-<script src="<?= asset('js/adminlte.min.js') ?>"></script>
-
-<script src="<?= asset('js/adminlte.min.js') ?>"></script>
-
-<!--axios-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.0/axios.min.js"
-        integrity="sha512-WrdC3CE9vf1nBf58JHepuWT4x24uTacky9fuzw2g/3L9JkihgwZ6Cfv+JGTtNyosOhEmttMtEZ6H3qJWfI7gIQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<!--sweetalert-->
-<script src="<?= asset('plugins/sweetalert2/sweetalert2.all.js') ?>"></script>
-
-<!-- toastr-->
-<script src="<?= asset('plugins/toastr/toastr.min.js') ?>"></script>
--- App/View/static/header.php
+++ App/View/static/header.php
@@ -16,4 +16,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Theme style -->
     <link rel="stylesheet" href="<?= asset('css/adminlte.min.css') ?>">
     <link rel="stylesheet" href="<?= asset('plugins/toastr/toastr.min.css') ?>">
+
+    <!-- jQuery -->
+    <script src="<?= asset('plugins/jquery/jquery.min.js') ?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= asset('plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= asset('js/adminlte.min.js') ?>"></script>
+
+    <script src="<?= asset('js/adminlte.min.js') ?>"></script>
+
+    <!--axios-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.0/axios.min.js"
+            integrity="sha512-WrdC3CE9vf1nBf58JHepuWT4x24uTacky9fuzw2g/3L9JkihgwZ6Cfv+JGTtNyosOhEmttMtEZ6H3qJWfI7gIQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!--sweetalert-->
+    <script src="<?= asset('plugins/sweetalert2/sweetalert2.all.js') ?>"></script>
+
+    <!-- toastr-->
+    <script src="<?= asset('plugins/toastr/toastr.min.js') ?>"></script>
 </head>

75c08f8fe5012f0647399bf359454f3b755fd5ea
Svobodennn <melih.sarac@hotmail.com>
2023-12-16 21:32:53 +0300

    Sepetle ilgili ve sipariş oluşturmayla ilgili routing işlemleri. redirect için user siparişlerim sayfasına routing oluşturuldu.

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 $cms->router->before('GET|POST', '/', 'Middlewares\MiddlewareAuth@isLogin');
+$cms->router->before('GET|POST', '/order', 'Middlewares\MiddlewareAuth@isLogin');
 //$cms->router->before('GET|POST', '/login', 'Middlewares\MiddlewareAuth@isLogin');
 
 $cms->router->get('/', 'Controllers\Home@Index');
 
 $cms->router->post('/addToCart', 'Controllers\Home@AddToCart');
+$cms->router->post('/decreaseFromCart', 'Controllers\Order@decreaseFromCart');
+$cms->router->post('/removeFromCart', 'Controllers\Order@removeFromCart');
+
+$cms->router->post('/checkCoupon', 'Controllers\Order@checkCoupon');
+$cms->router->post('/finalizeOrder', 'Controllers\Order@finalizeOrder');
 
 // Login Page
 $cms->router->get('/login', 'Controllers\Auth@Index');
@@ -13,4 +19,6 @@ $cms->router->get('/login', 'Controllers\Auth@Index');
 $cms->router->post('/login', 'Controllers\Auth@Login');
 $cms->router->get('/logout', 'Controllers\Auth@Logout');
 
+$cms->router->get('/order', 'Controllers\Order@Index');
+$cms->router->get('/user/orders', 'Controllers\User@Orders');
 

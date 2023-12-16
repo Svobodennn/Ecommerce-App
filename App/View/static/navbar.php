b2178b665fd452c96f32b645e09b02dc0859ce7c
Svobodennn <melih.sarac@hotmail.com>
2023-12-16 21:30:33 +0300

    sepet eklendi

--- App/View/static/navbar.php
+++ App/View/static/navbar.php
@@ -9,40 +9,63 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
-        <li  class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-comments"></i>
-                <?php
-                $count = 0;
-                    foreach ($cartList as $product){
-                        $count += $product['quantity'];
-                    }
-                ?>
-                <span id="urunSayisi" class="badge badge-danger navbar-badge"> <?= $count ?>  </span>
-            </a>
-            <div id="cartContainer" class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <?php foreach ($cartList as $product): ?>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-<!--                        ÜRÜN FOTOSU EKLE -->
-                        <img src="https://picsum.photos/id/63/100/100" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                <?= $product['title'] ?>
-                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm"><?= $product['description'] ?></p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> <?= $product['quantity'] ?></p>
-                        </div>
+
+        <?php
+            if (_session('title') != 'Sepet'):
+        ?>
+                <li  class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="fa fa-shopping-basket"></i>
+                        <?php
+                        if (isset($cartList)){
+                            $count = 0;
+                            foreach ($cartList as $product){
+                                $count += $product['quantity'];
+                            }
+                        }
+                        ?>
+
+                        <span id="urunSayisi" class="badge badge-danger navbar-badge"> <?= @$count ?>  </span>
+
+                    </a>
+                    <div id="cartContainer" class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <?php if (isset($cartList)): foreach ($cartList as $product): ?>
+                            <a href="#" class="dropdown-item">
+                                <!-- Message Start -->
+                                <div class="media">
+                                    <!--                        ÜRÜN FOTOSU EKLE -->
+                                    <img src="https://picsum.photos/id/63/100/100" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                                    <div class="media-body">
+                                        <h3 class="dropdown-item-title">
+                                            <?= $product['title'] ?>
+                                            <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                                        </h3>
+                                        <p class="text-sm"><?= $product['description'] ?></p>
+                                        <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> <?= $product['quantity'] ?></p>
+                                    </div>
+                                </div>
+                                <!-- Message End -->
+                            </a>
+                            <div class="dropdown-divider"></div>
+                        <?php endforeach; endif;  ?>
+
+                        <?php
+                        if (isset($cartList)):
+                            ?>
+                            <a href="<?= _link('order') ?>" class="dropdown-item dropdown-footer">SİPARİŞİ GÖRÜNTÜLE</a>
+                        <?php
+                        else:
+                            ?>
+                            <a href="#" class="dropdown-item dropdown-footer bg-danger">Sepette Hiç ürün yok.</a>
+                        <?php
+                        endif;
+                        ?>
                     </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <?php endforeach; ?>
-                <a href="#" class="dropdown-item dropdown-footer">SİPARİŞİ GÖRÜNTÜLE</a>
-            </div>
-        </li>
+                </li>
+
+            <?php
+            endif;
+        ?>
         <li class="nav-item">
             <a class="nav-link" href="<?= _link('logout') ?>">
                 <i class="fa fa-sign-out-alt"></i> Log Out

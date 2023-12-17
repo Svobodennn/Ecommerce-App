1861764a9bcfd2734a928aee86fe4cdf912a6410
Svobodennn <melih.sarac@hotmail.com>
2023-12-18 00:00:55 +0300

    sepet login kontrolü, Order->Cart güncellemesi, ve siparişlerle ilgili işlemlerin routingi

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+// session login kontrolü
 $cms->router->before('GET|POST', '/', 'Middlewares\MiddlewareAuth@isLogin');
-$cms->router->before('GET|POST', '/order', 'Middlewares\MiddlewareAuth@isLogin');
+$cms->router->before('GET|POST', '/login', 'Middlewares\MiddlewareAuth@returnBack');
+$cms->router->before('GET|POST', '/cart', 'Middlewares\MiddlewareAuth@isLogin');
 //$cms->router->before('GET|POST', '/login', 'Middlewares\MiddlewareAuth@isLogin');
 
 $cms->router->get('/', 'Controllers\Home@Index');
 
+// sepet işlemleri
+$cms->router->get('/cart', 'Controllers\Cart@Index');
 $cms->router->post('/addToCart', 'Controllers\Home@AddToCart');
-$cms->router->post('/decreaseFromCart', 'Controllers\Order@decreaseFromCart');
-$cms->router->post('/removeFromCart', 'Controllers\Order@removeFromCart');
-
-$cms->router->post('/checkCoupon', 'Controllers\Order@checkCoupon');
-$cms->router->post('/finalizeOrder', 'Controllers\Order@finalizeOrder');
+$cms->router->post('/decreaseFromCart', 'Controllers\Cart@decreaseFromCart');
+$cms->router->post('/removeFromCart', 'Controllers\Cart@removeFromCart');
+$cms->router->post('/checkCoupon', 'Controllers\Cart@checkCoupon');
+$cms->router->post('/finalizeOrder', 'Controllers\Cart@finalizeOrder');
 
 // Login Page
 $cms->router->get('/login', 'Controllers\Auth@Index');
@@ -19,6 +22,14 @@ $cms->router->get('/login', 'Controllers\Auth@Index');
 $cms->router->post('/login', 'Controllers\Auth@Login');
 $cms->router->get('/logout', 'Controllers\Auth@Logout');
 
-$cms->router->get('/order', 'Controllers\Order@Index');
-$cms->router->get('/user/orders', 'Controllers\User@Orders');
-
+// user sayfası işlemleri
+$cms->router->mount('/user', function () use ($cms) {
+    $cms->router->before('GET|POST', '/.*', 'Middlewares\MiddlewareAuth@isLogin');
+    $cms->router->get('/', 'Controllers\User@Index');
+    $cms->router->post('/edit', 'Controllers\User@EditProfile');
+    $cms->router->post('/password', 'Controllers\User@ChangePassword');
+    $cms->router->get('/orders', 'Controllers\User@showOrders');
+    $cms->router->post('/orders/cancelOrder', 'Controllers\User@cancelOrder');
+    $cms->router->get('/orders/details/([0-9]+)', 'Controllers\User@OrderDetails');
+    $cms->router->get('/orders/details/print/([0-9]+)', 'Controllers\User@PrintDetails');
+});

8d29a39f7394c9cc3c196d489bfa0abf962bf49b
Svobodennn <melih.sarac@hotmail.com>
2023-12-18 14:55:41 +0300

    Türkçeleştirme ve sweetalert bug fix

--- App/View/auth/index.php
+++ App/View/auth/index.php
@@ -24,12 +24,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 <div class="login-box">
     <div class="login-logo">
-        <a href="javascript:void(0)"><b>CMS</b>Project</a>
+        <a href="javascript:void(0)"><b>Kahve</b>Dükkanı</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
         <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+            <p class="login-box-msg">Oturum açmak için giriş yapınız</p>
 
             <form action="" id="login" method="post">
                 <div class="input-group mb-3">
@@ -41,7 +41,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </div>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Password">
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Şifre">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
@@ -49,39 +49,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-6">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember">
-                                Remember Me
-                            </label>
-                        </div>
-                    </div>
                     <!-- /.col -->
-                    <div class="col-6">
+                    <div class="col-12">
                         <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
 
-            <!--                    <div class="social-auth-links text-center mb-3">-->
-            <!--                        <p>- OR -</p>-->
-            <!--                        <a href="#" class="btn btn-block btn-primary">-->
-            <!--                            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook-->
-            <!--                        </a>-->
-            <!--                        <a href="#" class="btn btn-block btn-danger">-->
-            <!--                            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+-->
-            <!--                        </a>-->
-            <!--                    </div>-->
-            <!-- /.social-auth-links -->
-
-            <!--                    <p class="mb-1">-->
-            <!--                        <a href="forgot-password.html">I forgot my password</a>-->
-            <!--                    </p>-->
-            <!--                    <p class="mb-0">-->
-            <!--                        <a href="register.html" class="text-center">Register a new membership</a>-->
-            <!--                    </p>-->
         </div>
         <!-- /.login-card-body -->
     </div>
@@ -119,12 +94,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             .then(res => {
                 if (res.data.redirect){
                     window.location.href = res.data.redirect;
+                } else {
+                    Swal.fire(
+                        res.data.title,
+                        res.data.msg,
+                        res.data.status
+                    )
                 }
-                Swal.fire(
-                    res.data.title,
-                    res.data.msg,
-                    res.data.status
-                )
             }).catch(err => {
             console.log(err)
         })

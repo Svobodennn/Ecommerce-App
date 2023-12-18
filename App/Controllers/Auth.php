bd73c706c4450eb99fbc8f83eaae490f17a3d5d7
Svobodennn <melih.sarac@hotmail.com>
2023-12-18 17:22:04 +0300

    Kayıt olma ve kontrolleri eklendi

--- App/Controllers/Auth.php
+++ App/Controllers/Auth.php
@@ -59,5 +59,47 @@ class Auth extends BaseController
         setcookie('cart', '', time() - 3600, '/');
         redirect('login');
     }
+    public function Register(){
+        $data = $this->request->post();
+
+
+        if (!$data['name'] || !$data['surname'] || !$data['emailRegister'] || !$data['passwordRegister'] || !$data['passwordRepeat']){
+            echo json_encode(['status' => 'error', 'title' => 'Hata!', 'msg' => 'Lütfen tüm bilgileri doldurunuz.']);
+            exit();
+        }
+
+        if (strlen($data['name']) > 60 || strlen($data['surname']) > 60 || strlen($data['emailRegister']) > 60 || strlen($data['passwordRegister']) > 60){
+            echo json_encode(['status' => 'error', 'title' => 'Hata!', 'msg' => 'Lütfen bilgilerinizi düzgün bir şekilde girdiğinizden emin olunuz.']);
+            exit();
+        }
+            // rakam ve özel karakter kontrolü
+        $pattern = '/^[a-zA-ZÇçĞğİıÖöŞşÜü\s]+$/u';
+        if (!preg_match($pattern,$data['name']) || !preg_match($pattern,$data['surname'])){
+            echo json_encode(['status' => 'error', 'title' => 'Hata!', 'msg' => 'Lütfen bilgilerinizi düzgün bir şekilde girdiğinizden emin olunuz.']);
+            exit();
+        }
+
+        if ($data['passwordRegister'] != $data['passwordRepeat']){
+            echo json_encode(['status' => 'error', 'title' => 'Hata!', 'msg' => 'Şifreler birbiriyle uyuşmuyor.']);
+            exit();
+        }
+
+        $model = new ModelAuth();
+
+        $checkUSer = $model->checkUser($data['emailRegister']);
+
+        if ($checkUSer){
+            $result = $model->registerUser($data);
+
+            if ($result){
+                echo json_encode(['status' => 'success', 'title' => 'Başarılı', 'msg' => 'Başarıyla kayıt oldunuz. Giriş yapmaya yönlendiriliyorsunuz.','redirect' => _link('login')]);
+            } else {
+                echo json_encode(['status' => 'error', 'title' => 'Hata!', 'msg' => 'Bir hata meydana geldi. Lütfen daha sonra tekrar deneyin']);
+            }
+        } else {
+            echo json_encode(['status' => 'error', 'title' => 'Hata!', 'msg' => 'Bu kullanıcı zaten kayıtlı!']);
+        }
+
+    }
 
 }

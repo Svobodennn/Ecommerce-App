2d3062aafeca2eee5db60d8514c13fbfd56a43b9
Svobodennn <melih.sarac@hotmail.com>
2023-12-18 14:56:07 +0300

    bug fix

--- App/View/cart/index.php
+++ App/View/cart/index.php
@@ -1,62 +1,9 @@
 <?php
 _sessionSet('title', 'Sepet');
 echo $data['header'];
-function generateCouponCode() {
-    // Generate a random alphanumeric string with at least 3 'T' letters together between two numbers
-    $randomString = generateRandomStringWithT(13); // Adjusted to make a total length of 13
-
-    // Combine the parts to create the coupon code
-    $coupon = $randomString;
-
-    return $coupon;
-}
-
-function generateRandomStringWithT($length) {
-    // Ensure at least 3 'T' letters together between two numbers
-    $randomString = '';
-    $tCount = 0;
-
-    while (strlen($randomString) < $length) {
-        $randomChar = rand(0, 1) ? 'T' : rand(0, 9);
-
-        if ($randomChar === 'T') {
-            $tCount++;
-        } elseif (is_numeric($randomChar)) {
-            // Check if there are at least 3 'T' letters between two numbers
-            if ($tCount >= 3) {
-                $randomString .= 'TTT'; // Append at least 3 'T' letters together
-                $tCount = 0;
-            }
-        } else {
-            $tCount = 0;
-        }
-
-        $randomString .= $randomChar;
-    }
-
-    return $randomString;
-}
-
-function generateCoupons($count) {
-    $coupons = [];
-
-    for ($i = 0; $i < $count; $i++) {
-        $coupon = generateCouponCode();
-        $coupons[] = $coupon;
-    }
-
-    return $coupons;
-}
-
-// Generate 10 coupons
-$generatedCoupons = generateCoupons(10);
-
-// Display the generated coupons
-foreach ($generatedCoupons as $coupon) {
-    echo $coupon . PHP_EOL;
-}
-//debug($data['products']);
 ?>
+
+
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
     <?= $data['navbar'] ?>
@@ -88,36 +35,51 @@ foreach ($generatedCoupons as $coupon) {
                         <div class="col-md-8 d-flex flex-wrap">
 
                             <?php
-                                if (isset($data['cartList'])):
-                                    $totalPrice = 0;
+                            if ($data['cartList'] && isset($data['cartList'])):
+                                $totalPrice = 0;
                                 foreach ($data['cartList'] as $key => $value):
 
                                     $totalPrice += $value['total_price'];
-                            ?>
-                            <div id="card_<?= $value['id']?>" class="card mb-3" style="max-width: 540px;">
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                        <img src="https://picsum.photos/id/63/400/400" class="img-fluid rounded-start" alt="...">
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title"><?= $value['title'] ?></h5>
-                                            <p class="card-text"><?= $value['description'] ?></p>
-                                            <div class="quantity-controls d-flex">
-                                                <button class="quantity-decrease btn btn-warning" onclick="decreaseQuantity(<?= $value['id'] ?>)">-</button>
-                                                <p class="card-text quantity"><span id="product_<?= $value['id'] ?>"><?= $value['quantity']?></span>  Adet </p>
-                                                <input id="total_stock_<?=$value['id']?>" type="hidden" value="<?= $value['total_stock'] ?>">
-                                                <button class="quantity-increase btn btn-success" onclick="increaseQuantity(<?= $value['id'] ?>)">+</button>
+                                    ?>
+                                    <div id="card_<?= $value['id'] ?>" class="card mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4">
+                                                <img src="https://picsum.photos/id/63/400/400"
+                                                     class="img-fluid rounded-start" alt="...">
                                             </div>
-                                            <p class="card-text">Birim fiyatı: <span id="price_<?= $value['id'] ?>"><?= $value['price'] ?></span> ₺</p>
-                                            <p class="card-text">Toplam fiyat: <span id="total_price_<?= $value['id']?>"><?= $value['total_price']?></span> ₺</p>
-                                            <button title="Ürünü Sil" onclick="removeProduct(<?= $value['id'] ?>)" class="float-right btn btn-danger"><i class="fa fa-trash"></i></button>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title"><?= $value['title'] ?></h5>
+                                                    <p class="card-text"><?= $value['description'] ?></p>
+                                                    <div class="quantity-controls d-flex">
+                                                        <button class="quantity-decrease btn btn-warning"
+                                                                onclick="decreaseQuantity(<?= $value['id'] ?>)">-
+                                                        </button>
+                                                        <p class="card-text quantity"><span
+                                                                    id="product_<?= $value['id'] ?>"><?= $value['quantity'] ?></span>
+                                                            Adet </p>
+                                                        <input id="total_stock_<?= $value['id'] ?>" type="hidden"
+                                                               value="<?= $value['total_stock'] ?>">
+                                                        <button class="quantity-increase btn btn-success"
+                                                                onclick="increaseQuantity(<?= $value['id'] ?>)">+
+                                                        </button>
+                                                    </div>
+                                                    <p class="card-text">Birim fiyatı: <span
+                                                                id="price_<?= $value['id'] ?>"><?= $value['price'] ?></span>
+                                                        ₺</p>
+                                                    <p class="card-text">Toplam fiyat: <span
+                                                                id="total_price_<?= $value['id'] ?>"><?= $value['total_price'] ?></span>
+                                                        ₺</p>
+                                                    <button title="Ürünü Sil"
+                                                            onclick="removeProduct(<?= $value['id'] ?>)"
+                                                            class="float-right btn btn-danger"><i
+                                                                class="fa fa-trash"></i></button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            <?php
-                            endforeach; endif;
+                                <?php
+                                endforeach; endif;
                             ?>
                         </div>
                         <div class="col-md-4">
@@ -128,19 +90,23 @@ foreach ($generatedCoupons as $coupon) {
                                 </div>
                                 <div class="offcanvas-body">
                                     <div class="row">
-                                        <p>Sepet tutarı: <span id="total_price"><?= $totalPrice ?></span>₺</p>
+                                        <p>Sepet tutarı: <span id="total_price"><?= $totalPrice ?? '0' ?></span>₺</p>
                                         <p>Kargo Ücreti(500₺ Üzeri Bedava! ): <span id="cargo">54.99</span>₺</p>
                                     </div>
                                     <div id="coupon_row" class="row">
                                         <label for="">Kupon</label>
                                         <input id="coupon" type="text" placeholder="Kupon giriniz">
-                                        <button class="btn btn-primary" id="couponButton" type="button" onclick="checkCoupon()">Gir</button>
+                                        <button class="btn btn-primary" id="couponButton" type="button"
+                                                onclick="checkCoupon()">Gir
+                                        </button>
                                     </div>
                                     <div id="summary_row" class="row">
                                         <p>Toplam ödenecek tutar: <span id="summary"></span></p>
                                     </div>
                                     <div class="row">
-                                        <button onclick="finalizeOrder()" class="btn btn-primary mt-2"> Siparişi Tamamla </button>
+                                        <button onclick="finalizeOrder()" class="btn btn-primary mt-2"> Siparişi
+                                            Tamamla
+                                        </button>
                                     </div>
 
 
@@ -152,7 +118,7 @@ foreach ($generatedCoupons as $coupon) {
 
 
                     <?php
-                    debug($data['cartList']);
+                    //                    debug($data['cartList']);
                     ?>
                 </div>
             </div>
@@ -178,16 +144,16 @@ foreach ($generatedCoupons as $coupon) {
 
     <script>
 
-        $( document ).ready(function() {
+        $(document).ready(function () {
             checkCargo()
         });
 
-        function finalizeOrder(){
+        function finalizeOrder() {
 
             $.ajax({
                 type: 'POST',
                 url: '<?=_link('finalizeOrder')?>',
-                success: function(response) {
+                success: function (response) {
                     let res = JSON.parse(response)
 
                     if (res.status === "success") {
@@ -217,40 +183,38 @@ foreach ($generatedCoupons as $coupon) {
                         });
 
 
-
                     } else {
                         toastr[res.status](res.title, res.msg)
-                        if (res.redirect){
+                        if (res.redirect) {
                             setTimeout(() => {
                                 window.location.href = res.redirect
                             }, 3000)
                         }
                     }
                 },
-                error: function(err) {
+                error: function (err) {
                     toastr[err.status](res.title, res.msg)
                 }
             })
         }
 
-        function checkCargo(){
+        function checkCargo() {
             let total_price = parseFloat($('#total_price').text())
 
             let cargo = $('#cargo')
             let cargo_price = 0
 
-            if (total_price>500){
+            if (total_price > 500) {
                 cargo.css('text-decoration', 'line-through');
                 cargo.css('color', 'red');
                 cargo_price = parseFloat(cargo.text())
 
                 summary(total_price)
-            } else if(total_price <= 0) {
+            } else if (total_price <= 0) {
                 cargo.text('0')
                 summary(total_price)
 
-            }
-            else {
+            } else {
                 cargo.css('text-decoration', 'none');
                 cargo_price = parseFloat(cargo.text())
                 summary(total_price, cargo_price)
@@ -258,40 +222,41 @@ foreach ($generatedCoupons as $coupon) {
             }
         }
 
-        function summary(totalPrice, cargo_price = 0, discount = false){
+        function summary(totalPrice, cargo_price = 0, discount = false) {
             let summary_element = $('#summary')
             var total_price = parseFloat(totalPrice)
 
-            if (!discount){
-            summary_element.text((total_price+cargo_price).toFixed(2))
+            if (!discount) {
+                summary_element.text((total_price + cargo_price).toFixed(2))
             } else {
-                if (total_price > 3000){
+                if (total_price > 3000) {
                     let percantage = 25;
                     let discountAmount = parseFloat((total_price * percantage) / 100)
                     let discountedTotal = parseFloat(total_price - discountAmount)
                     summary_element.text(discountedTotal.toFixed(2))
-                    return {'description': '3000₺ Üzeri 1 Kilo kahve hediye ve ' ,'discount_amount': discountAmount, 'percantege': percantage}
+                    return {
+                        'description': '3000₺ Üzeri 1 Kilo kahve hediye ve ',
+                        'discount_amount': discountAmount,
+                        'percantege': percantage
+                    }
 
-                }
-                else if (total_price > 2000 ) {
+                } else if (total_price > 2000) {
                     let percantage = 20;
                     let discountAmount = parseFloat((total_price * percantage) / 100)
                     let discountedTotal = parseFloat(total_price - discountAmount)
 
                     summary_element.text(discountedTotal.toFixed(2))
-                    return {'description': '2000₺ Üzeri ' ,'discount_amount': discountAmount, 'percantege': percantage}
+                    return {'description': '2000₺ Üzeri ', 'discount_amount': discountAmount, 'percantege': percantage}
 
-                }
-                else if (total_price > 1500 ) {
+                } else if (total_price > 1500) {
                     let percantage = 15;
                     let discountAmount = parseFloat((total_price * percantage) / 100)
                     let discountedTotal = parseFloat(total_price - discountAmount)
 
                     summary_element.text(discountedTotal.toFixed(2))
-                    return {'description': '1500₺ Üzeri ' ,'discount_amount': discountAmount, 'percantege': percantage}
+                    return {'description': '1500₺ Üzeri ', 'discount_amount': discountAmount, 'percantege': percantage}
 
-                }
-                else if (total_price > 1000 ) {
+                } else if (total_price > 1000) {
 
                     let percantage = 10;
                     let discountAmount = parseFloat((total_price * percantage) / 100)
@@ -299,14 +264,14 @@ foreach ($generatedCoupons as $coupon) {
 
                     summary_element.text(discountedTotal.toFixed(2))
 
-                    return {'description': '1000₺ Üzeri ' ,'discount_amount': discountAmount, 'percantege': percantage}
+                    return {'description': '1000₺ Üzeri ', 'discount_amount': discountAmount, 'percantege': percantage}
                 }
             }
 
 
         }
 
-        function checkCoupon(){
+        function checkCoupon() {
             let summary_row = $('#summary_row')
             let couponElement = $('#coupon')
             let couponButton = $('#couponButton')
@@ -319,7 +284,7 @@ foreach ($generatedCoupons as $coupon) {
                     type: 'POST',
                     url: '<?=_link('checkCoupon')?>',
                     data: {coupon: coupon},
-                    success: function(response) {
+                    success: function (response) {
                         var res = JSON.parse(response)
 
                         if (res.status === "success") {
@@ -327,10 +292,13 @@ foreach ($generatedCoupons as $coupon) {
                             couponButton.prop('disabled', true);
                             couponElement.prop('disabled', true);
                             toastr[res.status](res.title, res.msg)
-                            let discount = summary(total_price,0, true)
+                            let discount = summary(total_price, 0, true)
 
 
-                            var newItemP = $('<p>').text(discount.description + '%' + discount.percantege + ' İndirim! ').append($('<span>').css({'color': 'red', 'text-decoration': 'line-through'}).text(discount.discount_amount));
+                            var newItemP = $('<p>').text(discount.description + '%' + discount.percantege + ' İndirim! ').append($('<span>').css({
+                                'color': 'red',
+                                'text-decoration': 'line-through'
+                            }).text(discount.discount_amount));
                             summary_row.prepend(newItemP)
 
                         } else {
@@ -338,7 +306,7 @@ foreach ($generatedCoupons as $coupon) {
 
                         }
                     },
-                    error: function(err) {
+                    error: function (err) {
                         toastr[err.status](res.title, res.msg)
 
                     }
@@ -347,7 +315,7 @@ foreach ($generatedCoupons as $coupon) {
             }
         }
 
-        function removeProduct(product_id){
+        function removeProduct(product_id) {
             var id = product_id;
             var card = $(`#card_${product_id}`)
             var product_total_price = parseFloat($(`#total_price_${product_id}`).text())
@@ -360,14 +328,14 @@ foreach ($generatedCoupons as $coupon) {
             axios.post('<?= 'removeFromCart' ?>', formData)
                 .then(res => {
 
-                toastr[res.data.status](res.data.title, res.data.msg)
+                    toastr[res.data.status](res.data.title, res.data.msg)
 
-                if (res.data.status == 'success') {
-                    total_price.text((parseFloat(total_price.text()) - product_total_price).toFixed(2));
-                    checkCargo()
-                    card.remove();
-                }
-            }).catch(err => {
+                    if (res.data.status == 'success') {
+                        total_price.text((parseFloat(total_price.text()) - product_total_price).toFixed(2));
+                        checkCargo()
+                        card.remove();
+                    }
+                }).catch(err => {
                 console.log(err)
             })
         }
@@ -379,7 +347,6 @@ foreach ($generatedCoupons as $coupon) {
             var total_price_element = $(`#total_price_${productId}`);
             var stock = $(`#total_stock_${productId}`).val()
             var total_price = $('#total_price')
-
 
 
             if (currentQuantity > 1 && currentQuantity <= stock) {
@@ -463,7 +430,6 @@ foreach ($generatedCoupons as $coupon) {
 
             }
         }
-
 
 
     </script>
